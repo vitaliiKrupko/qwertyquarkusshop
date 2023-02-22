f9df764384354c97671faed8010ed5a3021b4653
Vitalii Krupko <vitalii.krupko@ligazakon.ua>
2023-02-22 19:37:15 +0200

    Move repositories to model package

--- src/main/java/ua/krupko/vitalii/qwertyquarkusshop/model/repository/CustomerRepository.java
+++ src/main/java/ua/krupko/vitalii/qwertyquarkusshop/model/repository/CustomerRepository.java
@@ -1,4 +1,4 @@
-package ua.krupko.vitalii.qwertyquarkusshop.repository;
+package ua.krupko.vitalii.qwertyquarkusshop.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
